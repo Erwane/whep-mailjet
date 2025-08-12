258b663bdf1abd5e554419c466caf20bb3f83e8b
Erwane <erwane@phea.fr>
2025-08-12 23:48:44 +0200

    Support new checkSecurity() Interface method.

--- tests/TestCase/MailjetTest.php
+++ tests/TestCase/MailjetTest.php
@@ -21,6 +21,13 @@ use WHEP\ProviderInterface;
 #[CoversClass(Mailjet::class)]
 class MailjetTest extends TestCase
 {
+    public function testCheckIp(): void
+    {
+        $p = Factory::provider('mailjet', ['client_ip' => '185.211.120.0']);
+        $p->process([]);
+        $this->assertTrue($p->securityChecked());
+    }
+
     public static function dataTypesMap(): array
     {
         return [
@@ -123,7 +130,7 @@ class MailjetTest extends TestCase
         $json = File::getContent($resource);
         $data = json_decode($json, true);
 
-        $p = Factory::provider('mailjet', ['client_ip' => '185.211.120.0']);
+        $p = Factory::provider('mailjet', ['check_ip' => false]);
         $p->process($data);
 
         $this->assertEquals($type, $p->getType());
